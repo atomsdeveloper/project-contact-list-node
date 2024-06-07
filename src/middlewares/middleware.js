27de4e37f6037b510f19a79237cea3c7a63e4cd5
renanns <atomsfrontenf@gmail.com>
2024-06-07 14:03:09 -0300

    feat: add globals middleware to all routes

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,19 +1,28 @@
+// Adicionando middleware para todas as rotas com as mensagens de erros ou seccess com Flash Messages
 exports.middlewareGlobal = (req, res, next) => {
-  res.locals.umaVariavelLocal = 'Este é o valor da variável local.';
+  res.locals.errors = req.flash('erros');
+  res.locals.success = req.flash('success');
+  res.locals.user = req.session.user;
   next();
 };
-  
-exports.outroMiddleware = (req, res, next) => {
-  next();
-};
-  
+
 exports.checkCsrfError = (err, req, res, next) => {
-  if(err && 'EBADCSRFTOKEN' === err.code) {
+  if (err) {
     return res.render('404');
   }
+  next();
 };
-  
+
 exports.csrfMiddleware = (req, res, next) => {
-   res.locals.csrfToken = req.csrfToken();
-   next();
+  res.locals.csrfToken = req.csrfToken();
+  next();
+};
+
+exports.loginRequired = (req, res, next) => {
+  if (!req.session.user) {
+    req.session.save(() => res.redirect('/'));
+    req.flash('errors', 'Você precisa fazer login.');
+    return;
+  }
+  next();
 };
