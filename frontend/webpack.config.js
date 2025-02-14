5ec093ca9f362acc7525f2c75d34d4e2d8b3d374
Renan Nascimento <atomsfrontend@gmail.com>
2025-02-14 15:18:06 -0300

    fix: output folder

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'production',
   entry: './frontend/main.js',
   output: {
-    path: path.resolve(__dirname, 'public', 'assets', 'js'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   module: {
