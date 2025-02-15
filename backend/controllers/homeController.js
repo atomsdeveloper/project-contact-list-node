1435d9a1ec78a8af9b507a58aa58005d3702d00b
Renan Nascimento <atomsfrontend@gmail.com>
2025-02-14 21:56:28 -0300

    fix: checking remove

--- backend/controllers/homeController.js
+++ backend/controllers/homeController.js
@@ -2,9 +2,6 @@ const Contato = require('../models/contatoModel');
 
 exports.index = async function (req, res) {
   const contatos = await Contato.buscarContatos();
-  if (!contatos) {
-    req.flash('error', 'Não há cadastros na agenda.');
-  }
 
   // Armazenando na sessão os resultados encontrados em contatos.
   req.session.contatos = contatos;
