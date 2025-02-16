f411c0149365ff0bc90abd0fd8d5306ea3d2b12a
Renan Nascimento <atomsfrontend@gmail.com>
2025-02-16 20:13:06 -0300

    feat: refactored controllers to login and register

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -1,29 +1,22 @@
 const Login = require('../models/loginModel');
 
-// exports.register = async function (req, res) {
-//   try {
-//     const login = new Login(req.body);
-//     await login.register();
+exports.register = async function (req, res) {
+  try {
+    const login = new Login(req.body);
+    await login.register();
 
-//     // caso exista erros e enviada uma mensagem flash com o erro especifico.
-//     if (login.errors.length > 0) {
-//       req.flash('errors', login.errors);
-//       req.session.save(function () {
-//         return res.redirect('./index');
-//       });
-//       return;
-//     }
+    // caso exista erros e enviada uma mensagem com o erro especifico.
+    if (login.errors.length > 0) {
+      return res.status(400).json({ success: false, errors: login.errors });
+    }
 
-//     req.flash('success', 'Cadastro efetuado com sucesso, faça o Login.');
-//     req.session.save(function () {
-//       return res.redirect('./index');
-//     });
-//     return;
-//   } catch (e) {
-//     console.log(e);
-//     res.status(500).json({ message: "Error 404" });
-//   }
-// };
+    // caso não exista erros retona uma mensagem de sucesso.
+    return res.status(201).json({ errors: false, success: login.success });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "Error 404" });
+  }
+};
 
 exports.login = async function (req, res) {
   try {
@@ -36,15 +29,19 @@ exports.login = async function (req, res) {
     }
 
     // Após o Login com sucesso, armazenar o usuário na sessão
-    req.session.user = login.user;
-    return res.status(200).json({ message: "Você entrou no sistema.", user: login.user });
+    req.session.user = {
+      id: login.user._id,
+      email: login.user.email,
+      token: login.user.token, // Adicione o token aqui
+    };
+    return res.status(200).json({ errors: false, success: login.success, user: login.user });
   } catch (e) {
     console.log(e);
     return res.status(500).json({ message: "Error 404" });
   }
 };
 
-// exports.logout = function (req, res) {
-//   req.session.destroy();
-//   res.redirect('/');
-// };
+exports.logout = function (req, res) {
+  req.session.destroy();
+  res.redirect('/');
+};
