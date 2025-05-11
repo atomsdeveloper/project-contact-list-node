60f2ce8899351fdbf2ff682b7474e760ef28ea4a
Renan Nascimento <atomsfrontend@gmail.com>
2025-05-10 23:07:32 -0300

    fix: remove token

--- backend/controllers/homeController.js
+++ backend/controllers/homeController.js
@@ -3,7 +3,7 @@ const Contato = require('../models/contatoModel');
 exports.start = async function (req, res) {
   try {
     const contatos = await Contato.buscarContatos();
-    return res.status(200).json({ contatos, csrfToken: res.locals.csrfToken });
+    return res.status(200).json({ contatos });
   } catch (error) {
     return res
       .status(500)
